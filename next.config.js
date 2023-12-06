2a549cf41987cf1bfcfc024a68957d6fd1f99614
Rannie Raymundo <tzurany@gmail.com>
2023-12-06 17:36:54 +0800

    initial commit - fixing build issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: 'export'
+  basePath: process.env.NODE_ENV === 'production' ? '/ran-cv' : '',
+  output: 'export',
+  distDir: 'dist'
 }
 
 module.exports = nextConfig
